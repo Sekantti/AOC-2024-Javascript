dda648d680b9bf299b175f3113e0971b3d801ac4
Sekantti <k.randomletters@gmail.com>
2024-12-02 14:32:46 +0000

    day 2 code 2

--- problem2.js
+++ problem2.js
@@ -6,22 +6,22 @@ const content = readFileSync(filePath, 'utf-8');
 
 const reports = content.split('\n').map(levels => levels.split(" ").map(num => parseInt(num)));
 
-console.log(reports);
+//console.log(reports);
 
-/*const reportsLength = reports.length;
-const levelsAmount = reports[0].length;
+const reportsLength = reports.length;
 
 var safe = reportsLength;
 
 for (var i = 0; i < reportsLength; i++) {
     var increasing = (reports[i][0] - reports[i][1] < 0 == true);
+    var levelsAmount = reports[i].length;
     for (var j = 0; j < levelsAmount-1; j++) {
         var diff = reports[i][j] - reports[i][j+1];
-        if (Math.abs(diff) > 3 || increasing != (diff < 0)) {
+        if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
             safe--;
             break;
         }
     }
-}*/
+}
 
-//console.log(safe);
+console.log(safe);
