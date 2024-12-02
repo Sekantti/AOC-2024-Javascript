7921eaba8dfd163fb7696de295a1a32e76b73530
Sekantti <k.randomletters@gmail.com>
2024-12-02 19:32:38 +0000

    day 2 commit 4

--- problem2-2.js
+++ problem2-2.js
@@ -11,45 +11,32 @@ const reportsLength = reports.length;
 
 var safe = reportsLength;
 
-/*for (var i = 0; i < reportsLength; i++) {
-    var dampener = 0;
-    var levelsAmount = reports[i].length
-    for (var j = 0; j < levelsAmount-1; j++) {
-        var diff = reports[i][j] - reports[i][j+1];
-        var diff1 = reports[i][j] - reports[i][j+1];
-        try {
-            var diff2 = reports[i][j+1] - reports[i][j+2];
-        } catch (err) {
-            var diff2 = diff1;
-        }
-        if (diff == 0 || Math.abs(diff) > 3 || Math.sign(diff1) != Math.sign(diff2)) {
-            dampener++;
-        }
-        if (dampener > 1) {
-            safe--;
-            break;
-        }
-    }
-}*/
-
 for (var i = 0; i < reportsLength; i++) {
-    var dampener = 0;
     var levelsAmount = reports[i].length;
     var increasing = true;
-    if (Math.sign(reports[i][0] - reports[i][1]) != Math.sign(reports[i][1] - reports[i][2])) {
-        increasing = Math.sign(reports[i][0] - reports[i][1])
-        //Math.sign gives -1 or 1 as a result; if reports[i][j] - reports[i][j+1] is negative, then it's increasing
+    var increasing1 = reports[i][0] - reports[i][1];
+    var increasing2 = reports[i][1] - reports[i][2];
+    var increasingFinal = reports[i][levelsAmount-2] - reports[i][levelsAmount-1];
+    if (Math.sign(increasing1) != Math.sign(increasing2)) {
+        if (increasing1*increasingFinal > 0) {
+            increasing = increasing1 < 0;
+        } else if (increasing2*increasingFinal == 1) {
+            increasing = increasing2 < 2;
+        }
+    } else {
         increasing = reports[i][0] - reports[i][1] < 0;
-    }    
+    }
     for (var j = 0; j < levelsAmount-1; j++) {
         var diff = reports[i][j] - reports[i][j+1];
         if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
-            dampener++;
-        }
-        if (dampener > 1) {
-            safe--;
-            break;
-        }
+            var copy = reports[i];
+            copy.splice(j,1);
+            console.log(copy);
+/*            if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
+                safe--;
+                break;
+            }
+*/        }
     }
 }
 
