1d11309d5cb0a067d1614cde3f98a68b73c609f2
Sekantti <k.randomletters@gmail.com>
2024-12-03 15:47:20 +0000

    day 3 commit 2

--- problem3-2.js
+++ problem3-2.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 const filePath = 'problem3.in';
 
 const content = readFileSync(filePath, 'utf-8');
-var multiples = content.match(/mul\(\d{1,3},\d{1,3}\)/g);
+var multiples = content.replace(/\n/g,".").replace(/don't\(\)(.+?)do\(\)/g,".").match(/mul\(\d{1,3},\d{1,3}\)/g);
 
 const multiplesLength = multiples.length;
 const regexpFactors = /\d+/g;
