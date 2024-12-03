7e51379217d62c3420166db8cd324fdd338857ae
Sekantti <k.randomletters@gmail.com>
2024-12-03 20:10:19 +0000

    replaced for loops with for each

--- problem3.js
+++ problem3.js
@@ -5,13 +5,12 @@ const filePath = 'problem3.in';
 const content = readFileSync(filePath, 'utf-8');
 var multiples = content.match(/mul\(\d{1,3},\d{1,3}\)/g);
 
-const multiplesLength = multiples.length;
 const regexpFactors = /\d+/g;
 var sum = 0;
 
-for (var i = 0; i < multiplesLength; i++) {
-    var factors = multiples[i].match(regexpFactors);
-    sum+=factors[0]*factors[1];
-}
+multiples.forEach((val) => {
+    var factors = val.match(regexpFactors);
+    sum += factors[0]*factors[1];
+});
 
 console.log(sum);
