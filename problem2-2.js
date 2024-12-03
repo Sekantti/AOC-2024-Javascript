0673dc1f29af047650510f3b467602d5c60b078c
Sekantti <k.randomletters@gmail.com>
2024-12-03 08:36:19 +0000

    day 2 commit FINAL

--- problem2-2.js
+++ problem2-2.js
@@ -1,5 +1,4 @@
 import { readFileSync } from 'fs';
-import { eventNames } from 'process';
 
 const filePath = 'problem2.in';
 
@@ -12,31 +11,24 @@ const reportsLength = reports.length;
 var safe = reportsLength;
 
 for (var i = 0; i < reportsLength; i++) {
-    var levelsAmount = reports[i].length;
-    var increasing = true;
-    var increasing1 = reports[i][0] - reports[i][1];
-    var increasing2 = reports[i][1] - reports[i][2];
-    var increasingFinal = reports[i][levelsAmount-2] - reports[i][levelsAmount-1];
-    if (Math.sign(increasing1) != Math.sign(increasing2)) {
-        if (increasing1*increasingFinal > 0) {
-            increasing = increasing1 < 0;
-        } else if (increasing2*increasingFinal == 1) {
-            increasing = increasing2 < 2;
-        }
-    } else {
-        increasing = reports[i][0] - reports[i][1] < 0;
-    }
-    for (var j = 0; j < levelsAmount-1; j++) {
-        var diff = reports[i][j] - reports[i][j+1];
-        if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
-            var copy = reports[i];
-            copy.splice(j,1);
-            console.log(copy);
-/*            if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
-                safe--;
+    const levelsAmount = reports[i].length;
+    var variants = [];
+    var successes = levelsAmount;
+    for (var j = 0; j < levelsAmount; j++) {
+        variants[j] = [...reports[i]];
+        variants[j].splice(j, 1);
+        var increasing = variants[j][0]-variants[j][1] < 0;
+        for (var k = 0; k < levelsAmount - 2; k++) {
+            var diff = variants[j][k]-variants[j][k+1];
+            if (diff == 0 || Math.abs(diff) > 3 || increasing != (diff < 0)) {
+                successes--;
                 break;
             }
-*/        }
+        }
+        if (successes == 0) {
+            safe--;
+            break;
+        }
     }
 }
 
