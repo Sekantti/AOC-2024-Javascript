4fd57c0978e7dcca86fe4c9faae3500037b300bd
Sekantti <k.randomletters@gmail.com>
2024-12-08 15:16:34 +0100

    no more accessing global variables within functions

--- day 8/problem8-1.js
+++ day 8/problem8-1.js
@@ -1,16 +1,15 @@
 import { readFileSync } from 'fs';
 
-const searchSpace = [];
+const searchSpaceGlbl = [];
 
 readFileSync('problem8.in', 'utf-8').split('\n').forEach((row, i) => {
-    searchSpace[i] = row.split('');
+    searchSpaceGlbl[i] = row.split('');
 });
 
-const antennas = searchSpace.flat().filter((element) => element !== '.').filter((element, i, antennas) => antennas.indexOf(element) === i);
-const antinodes = new Set();
+const antennas = searchSpaceGlbl.flat().filter((element) => element !== '.').filter((element, i, antennas) => antennas.indexOf(element) === i);
 
 const indices = antennas.map((antenna) => {
-    return searchSpace.flatMap((row, i) =>
+    return searchSpaceGlbl.flatMap((row, i) =>
         row.map((element, j) => (element === antenna ? [i, j] : null))
     ).filter(item => item !== null)
 });
@@ -22,7 +21,7 @@ function addAntinode(pair, set) {
     }
 }
 
-function inbounds(node) {
+function inbounds(node, searchSpace) {
     return node[0] >= 0 && node[1] >= 0 && node[0] < searchSpace.length && node[1] < searchSpace.length;
 }
 
@@ -30,18 +29,23 @@ function newAntinode(i, j, node) {
     return [node[0] + i, node[1] + j];
 }
 
-indices.forEach((antenna) => {
-    antenna.forEach((location) => {
-        for (let i = 0; i < antenna.length; i++) {
-            if (location + "" !== antenna[i] + "") {
-                const distance = [location[0] - antenna[i][0], location[1] - antenna[i][1]]
-                const antinode = newAntinode(distance[0], distance[1], location)
-                if (inbounds(antinode)) {
-                    addAntinode(antinode, antinodes);
+function findAntinodes() {
+    const antinodes = new Set();
+    const searchSpace = searchSpaceGlbl;
+    indices.forEach((antenna) => {
+        antenna.forEach((location) => {
+            for (let i = 0; i < antenna.length; i++) {
+                if (location + "" !== antenna[i] + "") {
+                    const distance = [location[0] - antenna[i][0], location[1] - antenna[i][1]]
+                    const antinode = newAntinode(distance[0], distance[1], location)
+                    if (inbounds(antinode, searchSpace)) {
+                        addAntinode(antinode, antinodes);
+                    }
                 }
             }
-        }
+        })
     })
-})
+    return antinodes;
+}
 
-console.log(antinodes.size)
+console.log(findAntinodes().size)
--- day 8/problem8-2.js
+++ day 8/problem8-2.js
@@ -1,16 +1,14 @@
 import { readFileSync } from 'fs';
 
-const searchSpace = [];
+const searchSpaceGlbl = [];
 
 readFileSync('problem8.in', 'utf-8').split('\n').forEach((row, i) => {
-    searchSpace[i] = row.split('');
+    searchSpaceGlbl[i] = row.split('');
 });
 
-const antennas = searchSpace.flat().filter((element) => element !== '.').filter((element, i, antennas) => antennas.indexOf(element) === i);
-const antinodes = new Set();
-
+const antennas = searchSpaceGlbl.flat().filter((element) => element !== '.').filter((element, i, antennas) => antennas.indexOf(element) === i);
 const indices = antennas.map((antenna) => {
-    return searchSpace.flatMap((row, i) =>
+    return searchSpaceGlbl.flatMap((row, i) =>
         row.map((element, j) => (element === antenna ? [i, j] : null))
     ).filter(item => item !== null)
 });
@@ -22,7 +20,7 @@ function addAntinode(pair, set) {
     }
 }
 
-function inbounds(node) {
+function inbounds(node, searchSpace) {
     return node[0] >= 0 && node[1] >= 0 && node[0] < searchSpace.length && node[1] < searchSpace.length;
 }
 
@@ -30,12 +28,12 @@ function newAntinode(i, j, node) {
     return [node[0] + i, node[1] + j];
 }
 
-function calculateAntinodes(distance, node, antinodes) {
+function calculateAntinodes(distance, node, antinodes, searchSpace) {
     const max = Math.ceil(searchSpace.length/Math.hypot(distance[0], distance[1]))+1
     addAntinode(node, antinodes);
     for (let i = 0; i < max; i++) {
         let antinode = newAntinode(i*distance[0], i*distance[1], node);
-        if (inbounds(antinode)) {
+        if (inbounds(antinode, searchSpace)) {
             addAntinode(antinode, antinodes);
         } else {
             break;
@@ -43,15 +41,20 @@ function calculateAntinodes(distance, node, antinodes) {
     }
 }
 
-indices.forEach((antenna) => {
-    antenna.forEach((location) => {
-        for (let i = 0; i < antenna.length; i++) {
-            if (location + "" !== antenna[i] + "") {
-                const distance = [location[0] - antenna[i][0], location[1] - antenna[i][1]]
-                calculateAntinodes(distance, location, antinodes);
+function findAntinodes() {
+    const antinodes = new Set();
+    const searchSpace = searchSpaceGlbl;
+    indices.forEach((antenna) => {
+        antenna.forEach((location) => {
+            for (let i = 0; i < antenna.length; i++) {
+                if (location + "" !== antenna[i] + "") {
+                    const distance = [location[0] - antenna[i][0], location[1] - antenna[i][1]]
+                    calculateAntinodes(distance, location, antinodes, searchSpace);
+                }
             }
-        }
+        })
     })
-})
+    return antinodes;
+}
 
-console.log(antinodes.size);
+console.log(findAntinodes().size);
