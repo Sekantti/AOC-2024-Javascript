0ae354fd7bb53c833d8ce3249435c50f83c00232
Sekantti <k.randomletters@gmail.com>
2024-12-16 23:11:00 +0100

    problem 16-2 partial

---
+++ day 16/problem16-2.js
@@ -0,0 +1,94 @@
+import { readFileSync } from 'fs';
+
+const map = []
+readFileSync('problem16-example.in', 'utf-8').split('\n').forEach((row, i) => {
+    map[i] = row.split('');
+});
+
+const initialPosition = []
+map.forEach((row) => {
+    row.find((position) => {
+        if (position === 'S') {
+            initialPosition[0] = map.indexOf(row)
+            initialPosition[1] = row.indexOf(position);
+        }
+    })
+});
+
+const pq = []
+
+function insert(location, direction, cost) {
+    pq.push([cost, location, direction])
+}
+
+function priority_pop() {
+    pq.sort((a, b) => { return b[0] - a[0]})
+    const [cost, location, direction] = pq.pop()
+    return [location, direction, cost]
+}
+
+function turnClockwise(direction) { //expects direction input as array
+    const clockwiseTurn = {
+        "-1,0": [0, 1],
+        "0,1" : [1, 0],
+        "1,0": [0, -1],
+        "0,-1" : [-1, 0]
+
+    }
+    return clockwiseTurn[direction + '']
+}
+
+function turnAntiClockwise(direction) { //expects direction input as array
+    const antiClockwiseTurn = {
+        "-1,0": [0, -1],
+        "0,1" : [-1, 0],
+        "1,0": [0, 1],
+        "0,-1" : [1, 0]
+
+    }
+    return antiClockwiseTurn[direction + '']
+}
+
+
+function solve(location, direction, map) {
+    insert(location, direction, 0);
+
+    const seen = {}
+    let minCost = 0
+    while (true) {
+        const [[x, y], [dx, dy], cost] = priority_pop()
+        if (cost > minCost) {
+            break;
+        }
+        if ([[x,y], [dx, dy]] in seen) {
+            continue;
+        }
+    
+        if (map[x][y] === '#') {
+            continue;
+        }
+        if (map[x][y] === 'E') {
+            if (minCost === 0) {
+                minCost = cost
+            }
+            const [nextLoc, nextDir, nextcost] = priority_pop()
+            if (nextcost <= minCost) {
+                insert(nextLoc, nextDir, nextcost)
+                continue;
+            }
+            break;
+        }
+    
+        const [dxa, dya] = turnAntiClockwise([dx, dy])
+        const [dxc, dyc] = turnClockwise([dx, dy])
+        insert([x+dx, y+dy], [dx, dy], cost + 1)
+        insert([x+dxc, y+dyc], [dxc, dyc], cost + 1001)
+        insert([x+dxa, y+dya], [dxa, dya], cost + 1001)
+        seen[[[x,y], [dx,dy]]] = {
+            path: [[[x-dx, y-dy], [dx, dy]]],
+            cost: cost
+        };
+    }
+}
+
+console.log(solve(initialPosition, [0, 1], map))
